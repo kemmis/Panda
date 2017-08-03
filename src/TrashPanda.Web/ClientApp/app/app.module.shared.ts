ffcb6804e60e8e4e8af295a471581dc0f4ced99b
Rafe Kemmis <rafe@kemmis.info>
2017-08-03 18:34:01 -0400

    removed boilerplate junk

--- src/TrashPanda.Web/ClientApp/app/app.module.shared.ts
+++ src/TrashPanda.Web/ClientApp/app/app.module.shared.ts
@@ -5,18 +5,12 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { PostComponent } from "./components/post/post.component";
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         HomeComponent,
         PostComponent
     ],
@@ -25,12 +19,9 @@ import { PostComponent } from "./components/post/post.component";
         HttpModule,
         FormsModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            { path: '', component: HomeComponent },
             { path: 'post/:slug', component: PostComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: '**', redirectTo: '' }
         ])
     ]
 })
