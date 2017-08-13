0f3fd520ff154d5ae01df6df850ef309ccc0a779
Rafe Kemmis <rafe@kemmis.info>
2017-08-13 15:06:34 -0400

    comment link/anchor functionality

--- src/PandaPress.Web/ClientApp/app/components/post/post.component.ts
+++ src/PandaPress.Web/ClientApp/app/components/post/post.component.ts
@@ -1,15 +1,45 @@
 ﻿
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, AfterViewChecked } from '@angular/core';
 import { Post } from "../../models/post";
 import { PostComment } from "../../models/post-comment";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
     selector: 'post',
     templateUrl: './post.component.html'
 })
-export class PostComponent {
+export class PostComponent implements AfterViewChecked {
+
+    constructor(private activatedRoute: ActivatedRoute) { }
     @Input() post: Post;
     @Input() showComments: boolean = false;
+    private scrollExecuted: boolean = false;
+    ngAfterViewChecked(): void {
+        if (!this.scrollExecuted && this.showComments && this.post.id) {
+            let routeFragmentSubscription: Subscription;
+
+            // Automatic scroll
+            routeFragmentSubscription =
+                this.activatedRoute.fragment
+                    .subscribe(fragment => {
+                        if (fragment) {
+                            let element = document.getElementById(fragment);
+                            if (element) {
+                                element.scrollIntoView();
+                                this.scrollExecuted = true;
+
+                                // Free resources
+                                setTimeout(
+                                    () => {
+                                        routeFragmentSubscription.unsubscribe();
+                                    }, 1000);
+                            }
+                        }
+                    });
+        }
+    }
+
     onCommentCreated(comment: PostComment) {
         this.post.comments.push(comment);
     }
