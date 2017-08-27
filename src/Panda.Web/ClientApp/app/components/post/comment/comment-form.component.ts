3076e1e9c034b3688f11d340c62cfa5a40feaf88
Rafe Kemmis <rafe@kemmis.info>
2017-08-27 06:29:52 -0400

    recaptcha reset

--- src/Panda.Web/ClientApp/app/components/post/comment/comment-form.component.ts
+++ src/Panda.Web/ClientApp/app/components/post/comment/comment-form.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, OnDestroy, Input, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, EventEmitter, Output, ViewChild, NgZone } from '@angular/core';
 import { CommentService } from "../../../services/comment.service";
 import { CommentSaveRequest } from "../../../models/comment-save-request";
 import { PostComment } from "../../../models/post-comment";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+declare var grecaptcha: any;
 
 @Component({
     selector: 'comment-form',
@@ -11,19 +13,16 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 export class CommentFormComponent implements OnInit {
 
     constructor(private _commentService: CommentService,
-        private _formBuilder: FormBuilder) {
-        if (typeof window !== 'undefined') {
-            if (this.useReCaptcha) {
-                (<any>window).verifyCallback = this.verifyCallback.bind(this);
-            }
-        }
+        private _formBuilder: FormBuilder, private _zone: NgZone) {
     }
 
     form: FormGroup;
     @Input() postId: string;
 
+    _userReCaptcha: boolean;
     @Input() set useReCaptcha(value: boolean) {
         if (typeof window !== 'undefined') {
+            this._userReCaptcha = value;
             if (value) {
                 this.displayRecaptcha();
             }
@@ -62,11 +61,15 @@ export class CommentFormComponent implements OnInit {
         this._commentService.saveComment(newComment).subscribe((comment: PostComment) => {
             this.saving = false;
             this.form.patchValue({ text: "" });
+            if (this._userReCaptcha) {
+                grecaptcha.reset();
+            }
             this.commentCreated.emit(comment);
         });
     }
 
     displayRecaptcha() {
+        (<any>window).verifyCallback = this.verifyCallback.bind(this);
         var doc = <HTMLDivElement>document.getElementById('comment-form');
         var script = document.createElement('script');
         script.innerHTML = '';
